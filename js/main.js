279df857701b71ea78dcf1b04847513b961f701d
Elmar-Ibrahimli <elmarni@code.edu.az>
2021-04-23 17:09:44 +0400

    demo3 version

--- js/main.js
+++ js/main.js
@@ -26,48 +26,12 @@ $(document).ready(function () {
   });
 })
 
-
-// $('.owl-dot').on('click', function () {
-//       owl.trigger('stop.owl.autoplay');
-//   });
-
-//   $('.owl-carousel').data('owl.carousel').options.nav = false;
-//   $('.owl-carousel').trigger( 'refresh.owl.carousel' );
-
-
-
-
-//   $('.owl-dot').on('click', function () {
-//     $('.owl-carousel').trigger('stop.owl.autoplay');
-
-//     var carousel = $('.owl-carousel').data('owl.carousel');
-//     carousel.settings.autoplay = false; //don't know if both are necessary
-//     carousel.options.autoplay = false;
-//     $('.owl-carousel').trigger('refresh.owl.carousel');
-// });
-
-
-
-// var owl;
-
-// $(document).ready(function(){
-//     owl = $(".owl-carousel").owlCarousel({
-//         nav: true,
-//         autoplay: true,
-//     });
-
-//     owl.on('changed.owl.carousel', function(e) {
-//         owl.trigger('stop.owl.autoplay');
-//         owl.trigger('play.owl.autoplay');
-//     });
-// });
 var owl;
 
 $(document).ready(function () {
   owl = $(".owl-carousel").owlCarousel({
     nav: true,
     autoplay: true,
-    // autoplayTimeout: 3000
   });
 
   owl.on('changed.owl.carousel', function (e) {
@@ -76,32 +40,23 @@ $(document).ready(function () {
   });
 
   $('.hanburger-menu').click(function () {
-    $('.collapse').css("display","block");
+    $('.collapse').css("display", "block");
     $('.hanburger-menu').toggleClass('active-hamb');
     if ($('.hanburger-menu').hasClass('active-hamb')) {
       $('.mobile-ovarley').show();
-      $('.collapse').animate({ right: '0px' }, 300, 'linear');
-      $('.collapse').css("overflow", "hidden")
+      $('.collapse').animate({ right: '-40px' }, 300, 'linear');
+      $('.navbar').css("overflow", "visible")
     }
     else {
       $('.mobile-ovarley').hide();
       $('.collapse').animate({ right: '-100%' }, 300, 'linear');
-      // setTimeout(function() {
-      //   $('body').css('overflow', 'visible');
-      // }, 200);
-
+      $('.navbar').css("overflow", "hidden")
     }
   })
-   $(".mobile-ovarley").click(function(){
-        $('.hanburger-menu').removeClass('active-hamb');
-        $('.mobile-ovarley').hide();
-        $('.collapse').animate({ right: '-100%' }, 300, 'linear');
-      //   setTimeout(function() {
-      //     $('body').css('overflow', 'visible');
-      // }, 200);
-
-    });
-
- 
+  $(".mobile-ovarley").click(function () {
+    $('.hanburger-menu').removeClass('active-hamb');
+    $('.mobile-ovarley').hide();
+    $('.collapse').animate({ right: '-100%' }, 300, 'linear');
+  });
 });
 
